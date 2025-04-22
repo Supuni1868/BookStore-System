07a63afccd9d161adf1432187a1cdd19f5cd1aba
Supuni1868 <supuni.20231868@iit.ac.lk>
2025-04-22 23:02:47 +0530

    Created the Customer.java class.

--- src/main/java/com/bookstore/models/Customer.java
+++ src/main/java/com/bookstore/models/Customer.java
@@ -4,10 +4,67 @@
  */
 package com.bookstore.models;
 
-/**
- *
- * @author HI
- */
+
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Customer {
-    
+    private String email;
+    private String name;
+    private String password;
+    private List<CartItem> cartItems = new ArrayList<>();
+
+    public Customer() {}
+
+    public Customer(String email, String name, String password) {
+        this.email = email;
+        this.name = name;
+        this.password = password;
+        this.cartItems = new ArrayList<>();
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<CartItem> getCartItems() {
+        return cartItems;
+    }
+
+    public void setCartItems(List<CartItem> cartItems) {
+        this.cartItems = cartItems;
+    }
+
+    public void addToCart(CartItem item) {
+        this.cartItems.add(item);
+    }
+
+    public void removeFromCart(String isbn) {
+        cartItems.removeIf(item -> item.getIsbn().equals(isbn));
+    }
+
+    public void clearCart() {
+        cartItems.clear();
+    }
 }
