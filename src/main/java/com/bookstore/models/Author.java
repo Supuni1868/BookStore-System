e5a7eb6877a0c485feee44d0778b6b991eceb470
Supuni1868 <supuni.20231868@iit.ac.lk>
2025-04-22 22:30:37 +0530

    Created the author.java class.

---
+++ src/main/java/com/bookstore/models/Author.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.bookstore.models;
+
+
+
+public class Author {
+    private String name;
+    private String biography;
+
+    public Author() {}
+
+    public Author(String name, String biography) {
+        this.name = name;
+        this.biography = biography;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBiography() {
+        return biography;
+    }
+
+    public void setBiography(String biography) {
+        this.biography = biography;
+    }
+}
+
