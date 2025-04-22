01b716d3864fe448177bd877361e1caa788537bf
Supuni1868 <supuni.20231868@iit.ac.lk>
2025-04-22 22:35:10 +0530

    Created the Book.java class.

--- src/main/java/com/bookstore/models/Book.java
+++ src/main/java/com/bookstore/models/Book.java
@@ -4,10 +4,72 @@
  */
 package com.bookstore.models;
 
-/**
- *
- * @author HI
- */
+
+
 public class Book {
-    
+    private String isbn;
+    private String title;
+    private String author;
+    private int publicationYear;
+    private double price;
+    private int stockQuantity;
+
+    public Book() {}
+
+    public Book(String isbn, String title, String author, int publicationYear, double price, int stockQuantity) {
+        this.isbn = isbn;
+        this.title = title;
+        this.author = author;
+        this.publicationYear = publicationYear;
+        this.price = price;
+        this.stockQuantity = stockQuantity;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    public void setPublicationYear(int publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getStockQuantity() {
+        return stockQuantity;
+    }
+
+    public void setStockQuantity(int stockQuantity) {
+        this.stockQuantity = stockQuantity;
+    }
 }
