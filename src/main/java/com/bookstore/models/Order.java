988f19dcd0e1e702595efa29e4bf166905236b5c
Supuni1868 <supuni.20231868@iit.ac.lk>
2025-04-22 23:00:31 +0530

    Created the Order.java class.

--- src/main/java/com/bookstore/models/Order.java
+++ src/main/java/com/bookstore/models/Order.java
@@ -1,13 +1,54 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.bookstore.models;
 
-/**
- *
- * @author HI
- */
+
+import java.util.List;
+
 public class Order {
-    
+    private String orderId;
+    private String customerEmail;
+    private List<CartItem> items;
+    private double total;
+
+    public Order() {}
+
+    public Order(String orderId, String customerEmail, List<CartItem> items, double total) {
+        this.orderId = orderId;
+        this.customerEmail = customerEmail;
+        this.items = items;
+        this.total = total;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+
+    public List<CartItem> getItems() {
+        return items;
+    }
+
+    public void setItems(List<CartItem> items) {
+        this.items = items;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
 }
+
