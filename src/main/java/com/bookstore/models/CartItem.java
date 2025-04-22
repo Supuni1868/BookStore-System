66f0443db49d7c21a9f2de42f57939e099f83828
Supuni1868 <supuni.20231868@iit.ac.lk>
2025-04-22 23:15:04 +0530

    Created the CartItem.java class.

--- src/main/java/com/bookstore/models/CartItem.java
+++ src/main/java/com/bookstore/models/CartItem.java
@@ -4,10 +4,32 @@
  */
 package com.bookstore.models;
 
-/**
- *
- * @author HI
- */
+
+
 public class CartItem {
-    
+    private String isbn;
+    private int quantity;
+
+    public CartItem() {}
+
+    public CartItem(String isbn, int quantity) {
+        this.isbn = isbn;
+        this.quantity = quantity;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
